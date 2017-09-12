c8859f8fd1ee04ace2d7d6488c4eaa0b04dcf206
mikhailovve90 <mikhailov.ve90@gmail.com>
2017-09-12 18:43:29 +0300

    with mechanizm registation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,20 @@ class UsersController < ApplicationController
   def edit
   end
 
-  def show
+  def create
+    @user = User.new(user_params)
 
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегестрирован'
+    end
+  end
+
+
+  def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :username, :password, :password_confirmation, :avatar_url)
   end
 end
