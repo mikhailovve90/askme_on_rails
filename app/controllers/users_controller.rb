b1a9397c6a51cc1f8eae23d3fd3294ba92cd6890
mikhailovve90 <mikhailov.ve90@gmail.com>
2017-09-12 16:36:24 +0300

    with registration form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,16 @@
 class UsersController < ApplicationController
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Vadim',
-        username: 'installerro',
-        email: 'installerro@mail.ru',
-        avatar_url: 'http://illjuzija.ru/wp-content/uploads/2012/05/kartinka-so-skrytym-licom.jpg'
-      ),
-      User.new(
-        id: 2,
-        name: 'Misha',
-        username: 'aristofun',
-        email: 'aristofun@mail.ru'
-      )
-    ]
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def edit
   end
 
   def show
-    @user = User.new(
-      name: 'Vladimir',
-      email: 'xailo@list.ru',
-      username: 'vladimir',
-      # avatar_url: 'http://illjuzija.ru/wp-content/uploads/2012/05/kartinka-so-skrytym-licom.jpg'
-    )
 
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2017')),
-      Question.new(text: 'Читал "Государство"" Платона?', created_at: Date.parse('27.04.2017'))
-    ]
-
-    @new_question = Question.new
   end
 end
