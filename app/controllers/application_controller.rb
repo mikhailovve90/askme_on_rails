4b23eba66c5c684fa3be1ebc1900a9f8528dc666
mikhailovve90 <mikhailov.ve90@gmail.com>
2017-09-14 20:36:23 +0300

    cancel edit user for anonim and regect user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @currrent_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def reject_user
+    redirect_to root_path, alert: 'У вас нет прав на это действие'
+  end
 end
