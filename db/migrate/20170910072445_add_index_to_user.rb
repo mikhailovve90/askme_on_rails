01d5eeb261fc87376f5fbb2cc427852446520132
mikhailovve90 <mikhailov.ve90@gmail.com>
2017-09-17 16:39:37 +0300

    edit user index

--- db/migrate/20170910072445_add_index_to_user.rb
+++ db/migrate/20170910072445_add_index_to_user.rb
@@ -1,5 +1,5 @@
 class AddIndexToUser < ActiveRecord::Migration[5.1]
   def change
-    add_index :users, [:username, :email], unique: true
+    add_index :users, [:username], unique: true
   end
 end
