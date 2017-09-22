021cd9c84a55b2c515386522829c959623eed954
mikhailovve90 <mikhailov.ve90@gmail.com>
2017-09-22 17:55:14 +0300

    add edit user background color

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   validates :color , format: { with: /\A[a-fA-f0-9#]+\Z/ }
   validates :color , length: { maximum: 8 }
 
-  attr_accessor :password, #:color :avatar_url
+  attr_accessor :password #:color :avatar_url
 
   validates_presence_of :password, on: :create
   validates_confirmation_of :password
